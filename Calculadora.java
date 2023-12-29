0e59bb69d35d70c498597c0ca7ef7f58c855bb47
msuabar049 <miguel.angel.suarez@iescelia.org>
2023-12-29 20:44:57 +0100

    Incluyo pruebaCalculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,26 @@
+/*
+ * This is a class that performs basic operations.
+ * @param numero1 The first number of the operation.
+ * @param numero2 The second number of the operation.
+ * @param operacion The operation to be performed.
+ * @return The result of the operation.
+ * @see Calculadora
+ * @msuabar049
+ */
 public class Calculadora {
     private int numero1;
     private int numero2;
     private double resultado;
     private String operacion;
     
+    /* 
+     * This is the constructor of the class.
+     * @param numero1 The first number of the operation.
+     * @param numero2 The second number of the operation.
+     * @param operacion The operation to be performed.
+     * @see Calculadora
+     * @msuabar049
+    */
     public Calculadora(int numero1, int numero2, String operacion) {
         this.numero1 = numero1;
         this.numero2 = numero2;
--- pruebaCalculadora.java
+++ pruebaCalculadora.java
@@ -1,4 +1,15 @@
 public class pruebaCalculadora {
+    
+    /*
+     * This is the main method of the program.
+     * @param numero1 The first number of the operation.
+     * @param numero2 The second number of the operation.
+     * @param operacion The operation to be performed.
+     * @return The result of the operation.
+     * @see Calculadora
+     * @msuabar049
+     */
+     
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora(2, 3, "+");
         calculadora.realizarOperacion();
